e188b209f8ec7240f4193d37869f0a060f79dcbd
lmw6412036 <lmw6412036@sina.com>
2017-09-05 21:11:12 +0800

    Changes to be committed: 	modified:   package.json 	modified:   src/pages/index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,81 +1,104 @@
 <template>
-  <div class="page index">
-    <div class="wrapper">
-      <div class="banner swiper-container" ref="swiper">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">slider1</div>
-          <div class="swiper-slide">slider2</div>
-          <div class="swiper-slide">slider3</div>
+    <div class="page index">
+        <div class="wrapper">
+            <div class="banner swiper-container" ref="swiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" v-if="ad.userType=='PAT'" v-for="ad in adsettings">
+                        <img :src="ad.adUrl" alt="">
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
         </div>
-      </div>
+        <app-footer class="noflex"></app-footer>
+        <loading ref="loading"></loading>
     </div>
-    <app-footer class="noflex"></app-footer>
-    <loading ref="loading"></loading>
-  </div>
 </template>
 
 <script>
-  import http from "../lib/http"
-  import Loading from "../base/loading.vue";
-  import AppFooter from "../components/app-footer.vue"
+    import http from "../lib/http"
+    import Loading from "../base/loading.vue";
+    import AppFooter from "../components/app-footer.vue"
 
-  export default {
-    mixins: [],
-    data() {
-      return {};
-    },
-    computed: {},
-    components: {
-      Loading,
-      AppFooter
-    },
-    created() {
-      this.swiper = null;
-    },
-    mounted() {
-      this._swiper();
-      this._init();
-    },
-    beforeDestroy() {
+    export default {
+        mixins: [],
+        data() {
+            return {
+                adsettings: [],
+            };
+        },
+        computed: {},
+        components: {
+            Loading,
+            AppFooter
+        },
+        created() {
+            this.swiper = null;
+        },
+        mounted() {
+            this._init();
+        },
+        beforeDestroy() {
 
-    },
-    watch: {},
-    methods: {
-      _init() {
-        this.$refs.loading.show();
-        http("smarthos.user.pat.index", {})
-          .then((res) => {
-            this.$refs.loading.hide();
-            console.log(res);
-          });
-      },
-      _swiper() {
-        setTimeout((res) => {
-          this.swiper = new Swiper(this.$refs.swiper);
-        }, 20)
-      }
-    }
-  };
+        },
+        watch: {},
+        methods: {
+            _init() {
+                this.$refs.loading.show();
+                http("smarthos.user.pat.index", {})
+                    .then((res) => {
+                        this.$refs.loading.hide();
+                        console.log(res);
+                        if (res.code == 0) {
+                            let obj = res.obj;
+                            this.adsettings = obj.adsettings;
+                            this._swiper();
+                        }
+                    });
+            },
+            _swiper() {
+                setTimeout((res) => {
+                    this.swiper = new Swiper(this.$refs.swiper, {
+                        loop: true,
+                        autoplay: 5000,
+                        pagination: '.swiper-pagination',
+                    });
+                }, 20)
+            }
+        }
+    };
 </script>
 
-<style scoped lang="scss">
-  @import "../common/common";
+<style lang="scss">
+    @import "../common/common";
 
-  .index {
-    .wrapper {
+    .index {
+        .wrapper {
+            .banner {
+                $bannerHei: 350px;
+                height: $bannerHei;
+                overflow: hidden;
+                .swiper-slide {
+                    height: $bannerHei;
+                    > img {
+                        width: 100%;
+                        height: $bannerHei;
+                    }
+                }
+                .swiper-pagination {
+                    text-align: right;
+                    span {
+                        background-color: white;
+                        opacity: 0.8;
+                        &.swiper-pagination-bullet-active{
+                            background-color: $mainColor;
+                        }
+                    }
+                }
+            }
+        }
 
     }
-  }
 
-  .banner {
-    $bannerHei: 350px;
-    height: $bannerHei;
-    overflow: hidden;
-    > div {
-      height: $bannerHei;
-      > img {
-        height: $bannerHei;
-      }
-    }
-  }
+
 </style>
