cac2e223bbc941c44b2ceb5039e48590102fa99c
lmw6412036 <lmw6412036@sina.com>
2017-09-13 23:09:03 +0800

    modified:   src/lib/http.js 	modified:   src/pages/my/about.vue 	new file:   static/help.xml

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,23 +1,31 @@
 import axios from "axios";
 import config from "./config"
 
-export default function (service, options) {
-  let baseParams = config.base_params;
-  let data = {
-    service,
-    ...baseParams,
-    ...options
-  };
-  let axiosConfig = {
-    headers: {
-      "sign": "test",
-      "Content-Type": "application/json"
-    },
-  }
-  return axios.post(config.api_url, data, axiosConfig)
-    .then((res) => {
-      if (res.status == 200) {
-        return res.data;
-      }
-    });
+export default function (service, options, conf) {
+    let baseParams = config.base_params;
+    let data = {
+        service,
+        ...baseParams,
+        ...options
+    };
+    let axiosConfig = {
+        url: config.api_url,
+        method: "post",
+        headers: {
+            "sign": "test",
+            "Content-Type": "application/json"
+        },
+        data: data
+    }
+
+    if (conf) {
+        axiosConfig = Object.assign({}, axiosConfig, conf);
+    }
+
+    return axios(axiosConfig)
+        .then((res) => {
+            if (res.status == 200) {
+                return res.data;
+            }
+        });
 }
